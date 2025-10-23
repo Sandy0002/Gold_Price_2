f13e99c2adda47401351535eb3c70f86b469c76c
Sandeep <Sandy@Commited.com>
2025-10-23 22:35:42 +0530

    Created scripts for preprocessing data,building model and generating predictions

---
+++ src/train_model.py
@@ -0,0 +1,33 @@
+# In this program we will be training data by taking inputs from sequence_creator.py 
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+import matplotlib.pyplot as plt
+from src.sequence_creator import prepare_data
+
+
+def training():
+    # Getting data from sequence_creator
+    xTrain,xTest,yTrain,yTest,scaler = prepare_data()
+
+    model = Sequential([
+        # IN layer 1: 50 : Number of neurons, input_shape: timesteps,features, dropout is to remove some neurons randomly to avoid overfitting, return_sequence is for  outputs the entire sequence to the next LSTM layer (needed when stacking LSTMs).
+        LSTM(50, return_sequences=True, input_shape=(xTrain.shape[1], 1)), Dropout(0.2),
+        LSTM(50, return_sequences=False), Dropout(0.2),
+
+        # Fully connected layer with 25 neurons. ReLU helps capture non-linear relationships between past and future prices.
+        Dense(25, activation='relu'),
+        Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    # model.summary()
+
+
+    # ===== 5. Train =====
+    # here batch_size gives how many samples (sequences) the model processes before updating weights during training.
+    history = model.fit(xTrain, yTrain, epochs=20, batch_size=32, validation_data=(xTest, yTest), verbose=1)
+
+    model.save("models\\gold_lstm_model.h5")
+
+    return xTest,yTest,scaler
