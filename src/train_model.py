6daae3b8a28bbe2219e0ba8f4dc0b0a79d5e84c3
Sandeep <Sandy@Commited.com>
2025-10-24 08:31:30 +0530

    Updated data preprocessing file to use pathlib

--- src/train_model.py
+++ src/train_model.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 from src.sequence_creator import prepare_data
 from pathlib import Path
 from tensorflow.keras.models import load_model
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
-def modelSaver(newModel,xTest,yTest):
+def modelUpdater(newModel,xTest,yTest):
     project_root = Path(__file__).resolve().parents[1]
     # Create models directory under project root
     models_dir = project_root / "models"
@@ -18,20 +18,22 @@ def modelSaver(newModel,xTest,yTest):
 
     # Evaluate new model
     y_pred_new = newModel.predict(xTest)
-    new_score = mean_squared_error(yTest, y_pred_new)
+    new_score_mse = mean_squared_error(yTest, y_pred_new)
+    new_score_r2 = r2_score(yTest, y_pred_new)
 
     # Check if existing model exists
     if model_path.exists():
         # Load and evaluate old model
         old_model = load_model(model_path)
         y_pred_old = old_model.predict(xTest)
-        old_score = mean_squared_error(yTest, y_pred_old)
+        old_score_mse = mean_squared_error(yTest, y_pred_old)
+        old_score_r2 = r2_score(yTest, y_pred_old)
 
-        print(f"Old model MSE: {old_score:.5f}")
-        print(f"New model MSE: {new_score:.5f}")
+        print(f"Old model MSE: {old_score_mse:.5f}")
+        print(f"New model MSE: {new_score_mse:.5f}")
 
         # Compare and decide
-        if new_score < old_score:  # Lower MSE = better model
+        if new_score_r2 < old_score_r2:  # Lower MSE = better model
             print("✅ New model is better. Saving it.")
             newModel.save(model_path)
         else:
@@ -63,6 +65,6 @@ def training():
     
     newModel = model
     # Get project root (assuming current file is inside src/)
-    modelSaver(newModel,xTest,yTest)
+    modelUpdater(newModel,xTest,yTest)
 
     return xTest,yTest,scaler
